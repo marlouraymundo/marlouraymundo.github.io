983529261bbb661b8d75e35f0ee8e29614e9cedb
Marlou Raymundo <wtertwo@gmail.com>
2025-04-30 17:34:31 +0800

    Update script.js

--- script.js
+++ script.js
@@ -11,33 +11,3 @@ function toggleNav(){
     $("nav").fadeIn().css('display', 'flex');
   }
 }
-
-// when clicking + or ☰ button
-$("button").click(function(){
-  // when clicking ☰ button, open nav
-  if($("header").hasClass("open")){
-    toggleNav();
-  }
-  // when clicking + button, open header
-  else{
-    $("header").addClass("open");
-  }
-});
-
-// close nav
-$("#nav-close").click(function(){
-  toggleNav();
-});
-
-// scroll to sections
-$("nav li").click(function(){
-  // get index of clicked li and select according section
-  var index = $(this).index();
-  var target = $("content section").eq(index);
-  
-  toggleNav();
-  
-  $('html,body').delay(300).animate({
-    scrollTop: target.offset().top
-  }, 500);
-});
