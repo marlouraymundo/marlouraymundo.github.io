27e40430e187fed10594bad687d92f16a7debb2d
Marlou Raymundo <wtertwo@gmail.com>
2025-04-30 17:49:53 +0800

    430549

--- script.js
+++ script.js
@@ -1,10 +1,9 @@
-// When clicking the button, add "open" class to header to trigger animation
 $("button").click(function(){
-  if(!$("header").hasClass("open")){
+  if($("header").hasClass("open")){
+    // If header is open, remove the class to close it
+    $("header").removeClass("open");
+  } else {
+    // If header is closed, add the class to open it
     $("header").addClass("open");
   }
-  else if($("header").hasClass("open")){
-    $("header").addClass("");
-  }
 });
-
