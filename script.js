6e8cd6c1a3ed7d9dda9b491ffd4a30b41309f706
Marlou Raymundo <wtertwo@gmail.com>
2025-04-30 17:43:07 +0800

    543430

--- script.js
+++ script.js
@@ -1,43 +1,7 @@
-// function to open/close nav
-function toggleNav(){
-  // if nav is open, close it
-  if($("nav").is(":visible")){
-    $("nav").fadeOut();
-    $("button").removeClass("menu");
-  }
-  // if nav is closed, open it
-  else{
-    $("button").addClass("menu");
-    $("nav").fadeIn().css('display', 'flex');
-  }
-}
-
-// when clicking + or ☰ button
+// When clicking the button, add "open" class to header to trigger animation
 $("button").click(function(){
-  // when clicking ☰ button, open nav
-  if($("header").hasClass("open")){
-    toggleNav();
-  }
-  // when clicking + button, open header
-  else{
+  if(!$("header").hasClass("open")){
     $("header").addClass("open");
   }
 });
 
-// close nav
-$("#nav-close").click(function(){
-  toggleNav();
-});
-
-// scroll to sections
-$("nav li").click(function(){
-  // get index of clicked li and select according section
-  var index = $(this).index();
-  var target = $("content section").eq(index);
-  
-  toggleNav();
-  
-  $('html,body').delay(300).animate({
-    scrollTop: target.offset().top
-  }, 500);
-});
